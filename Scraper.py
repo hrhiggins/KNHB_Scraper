b04a9f8055d5e496ca712035aa06f77133300446
Harry <harry@jezuk.co.uk>
2024-04-22 09:57:20 +0200

    Began

--- Scraper.py
+++ Scraper.py
@@ -8,10 +8,18 @@ url = 'https://www.knhb.nl/match-center#/competitions/N7/results'
 page = requests.get(url)
 
 # create beautifulsoup object
-soup = BeautifulSoup(page.content)
+soup = BeautifulSoup(page.text, "html.parser")
 
-# find all table cells (with tag 'td')
-cells = soup.findALL('span')
-print(cells)
+# badges = soup.body.find('div', attrs={'class': 'badges'})
+#for span in badges.span.find_all('span', recursive=False):
+ #   print
+
+span_tags = soup.find_all('span')
+for span in span_tags:
+    print(span.text)
+
+#li_tags = soup.find_all('li')
+#for li in li_tags:
+    #print(li.text)
 
 
