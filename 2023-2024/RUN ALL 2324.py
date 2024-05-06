2f907b9edfc070deebfb1e509a587dfdfcbe87c0
Harry <harry@jezuk.co.uk>
2024-05-06 17:24:20 +0200

    Lots of changes

--- 2023-2024/RUN ALL 2324.py
+++ 2023-2024/RUN ALL 2324.py
@@ -16,7 +16,25 @@ try:
     print('H1 OK Scraper Complete (3/3)')
 
     print('All Programs Complete')
+
 except IOError:
-    with open(r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 OK Scraper 2324.py") as file:
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\D1 1K Scraper 2324.py"
+    ) as file:
+        exec(file.read())
+
+    # run H1 1K Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 1K Scraper 2324.py"
+    ) as file:
+        exec(file.read())
+    print('H1 1K Scraper Complete (2/3)')
+
+    # run H1 OK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 OK Scraper 2324.py"
+    ) as file:
         exec(file.read())
     print('H1 OK Scraper Complete (3/3)')
+
+    print('All Programs Complete')
--- 2024-2025/RUN ALL 2425.py
+++ 2024-2025/RUN ALL 2425.py
@@ -1,51 +1,125 @@
-# run D1 HK Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 HK Scraper 2425.py") as file:
-    exec(file.read())
-print('D1 HK Scraper Complete (1/10)')
+try:
+    # run D1 HK Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 HK Scraper 2425.py") as file:
+        exec(file.read())
+    print('D1 HK Scraper Complete (1/10)')
 
-# run D1 PK Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 PK Scraper 2425.py") as file:
-    exec(file.read())
-print('D1 PK Scraper Complete (2/10)')
+    # run D1 PK Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 PK Scraper 2425.py") as file:
+        exec(file.read())
+    print('D1 PK Scraper Complete (2/10)')
 
-# run D1 OK Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 OK Scraper 2425.py") as file:
-    exec(file.read())
-print('D1 OK Scraper Complete (3/10)')
+    # run D1 OK Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 OK Scraper 2425.py") as file:
+        exec(file.read())
+    print('D1 OK Scraper Complete (3/10)')
 
-# run D1 1K Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 1K Scraper 2425.py") as file:
-    exec(file.read())
-print('D1 1K Scraper Complete (4/10)')
+    # run D1 1K Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 1K Scraper 2425.py") as file:
+        exec(file.read())
+    print('D1 1K Scraper Complete (4/10)')
 
-# run D1 2K Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 2K Scraper 2425.py") as file:
-    exec(file.read())
-print('D1 2K Scraper Complete (5/10)')
+    # run D1 2K Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\D1 2K Scraper 2425.py") as file:
+        exec(file.read())
+    print('D1 2K Scraper Complete (5/10)')
 
-# run H1 HK Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 HK Scraper 2425.py") as file:
-    exec(file.read())
-print('H1 HK Scraper Complete (6/10)')
+    # run H1 HK Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 HK Scraper 2425.py") as file:
+        exec(file.read())
+    print('H1 HK Scraper Complete (6/10)')
 
-# run H1 PK Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 PK Scraper 2425.py") as file:
-    exec(file.read())
-print('H1 PK Scraper Complete (7/10)')
+    # run H1 PK Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 PK Scraper 2425.py") as file:
+        exec(file.read())
+    print('H1 PK Scraper Complete (7/10)')
 
-# run H1 OK Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 OK Scraper 2425.py") as file:
-    exec(file.read())
-print('H1 OK Scraper Complete (8/10)')
+    # run H1 OK Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 OK Scraper 2425.py") as file:
+        exec(file.read())
+    print('H1 OK Scraper Complete (8/10)')
 
-# run H1 1K Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 1K Scraper 2425.py") as file:
-    exec(file.read())
-print('H1 1K Scraper Complete (9/10)')
+    # run H1 1K Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 1K Scraper 2425.py") as file:
+        exec(file.read())
+    print('H1 1K Scraper Complete (9/10)')
 
-# run H1 2K Scraper
-with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 2K Scraper 2425.py") as file:
-    exec(file.read())
-print('H1 2K Scraper Complete (10/10)')
+    # run H1 2K Scraper
+    with open(r"C:\Users\Harry Higgins\PycharmProjects\pythonProject\2024-2025\H1 2K Scraper 2425.py") as file:
+        exec(file.read())
+    print('H1 2K Scraper Complete (10/10)')
 
-print('All Programs Complete')
+    print('All Programs Complete')
+
+except IOError:
+    # run D1 HK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\D1 HK Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('D1 HK Scraper Complete (1/10)')
+
+    # run D1 PK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\D1 PK Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('D1 PK Scraper Complete (2/10)')
+
+    # run D1 OK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\D1 OK Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('D1 OK Scraper Complete (3/10)')
+
+    # run D1 1K Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\D1 1K Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('D1 1K Scraper Complete (4/10)')
+
+    # run D1 2K Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\D1 2K Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('D1 2K Scraper Complete (5/10)')
+
+    # run H1 HK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 HK Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('H1 HK Scraper Complete (6/10)')
+
+    # run H1 PK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 PK Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('H1 PK Scraper Complete (7/10)')
+
+    # run H1 OK Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 OK Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('H1 OK Scraper Complete (8/10)')
+
+    # run H1 1K Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 1K Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('H1 1K Scraper Complete (9/10)')
+
+    # run H1 2K Scraper
+    with open(
+            r"C:\Users\Harry\OneDrive\University Work\S4\Project\GitHub\KNHB_Scraper\2023-2024\H1 2K Scraper 2425.py"
+    ) as file:
+        exec(file.read())
+    print('H1 2K Scraper Complete (10/10)')
+
+    print('All Programs Complete')
